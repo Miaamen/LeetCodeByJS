c21cb4cf248a1e78125428cd82a1d83b282f86d7
AMen <zhuaoxve@163.com>
2019-11-04 20:17:29 +0800

    add Q203 AND Q205

---
+++ easy/Q203.js
@@ -0,0 +1,23 @@
+/*203. 移除链表元素*/
+
+/*删除链表中等于给定值 val 的所有节点。
+
+示例:
+
+输入: 1->2->6->3->4->5->6, val = 6
+输出: 1->2->3->4->5*/
+
+var removeElements = function(head, val) {
+    while(head != null && head.val === val){
+        head = head.next;
+    }
+    var res = head; 
+    while(res != null){
+        if(res.next != null && res.next.val == val){
+            res.next = res.next.next;
+        }else{
+            res = res.next;
+        }
+    }
+    return head;
+};
