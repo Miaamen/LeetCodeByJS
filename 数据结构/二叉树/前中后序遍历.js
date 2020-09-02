65f8a770544a30e63a6681b88fdde809528d4034
Mia <42193533+Miaamen@users.noreply.github.com>
2020-09-02 09:29:08 +0800

    Update 前中后序遍历.js

--- 数据结构/二叉树/前中后序遍历.js
+++ 数据结构/二叉树/前中后序遍历.js
@@ -20,18 +20,18 @@ var inorderTraversal = function(root) {
 var inorderTraversal = function(root) {
     let result = []
     // 借助栈实现
-    let stack = [];
+    let stack = []
     while (stack.length > 0 || root !== null) {
         if (root) {
-            stack.push(root);
+            stack.push(root)
             root = root.left
         } else {
-            root = stack.pop();
-            result.push(root.val);
-            root = root.right;
+            root = stack.pop()
+            result.push(root.val)
+            root = root.right
         }
     }
-    return result;
+    return result
 }
 
 // 前序遍历 --- 递归实现
@@ -51,5 +51,57 @@ var preorderTraversal = function(root) {
     }
     handle(root)
     return result
-};
+}
+
 // 前序遍历 --- 迭代实现
+var preorderTraversal = function(root) {
+    let result = []
+    let stack = []
+    if (root) stack.push(root)  // 先root.val
+    while (stack.length) {
+        root = stack.pop()
+        result.push(root.val)
+        root.right && stack.push(root.right)
+        root.left && stack.push(root.left)
+    }
+    return result
+}
+
+// 后序遍历 --- 递归实现
+var postorderTraversal = function(root) {
+    let result = []
+    function handle(root){
+        if(root) {
+            if(root.left !== null) {
+                handle(root.left)
+            }
+            if(root.right !== null) {
+                handle(root.right)
+            }
+            result.push(root.val)
+        }
+    }
+    handle(root)
+    return result
+}
+
+// 后序遍历 --- 迭代实现
+var postorderTraversal = function(root) {
+    let result = []
+    let stack = []
+
+    if(root) {
+        stack.push(root)
+    }
+    while(stack.length > 0) {
+        let node = stack.pop()
+        result.unshift(node.val)
+        if(node.left !== null) {
+            stack.push(node.left)
+        }
+        if(node.right !== null) {
+            stack.push(node.right)
+        }
+    }
+    return result
+}
